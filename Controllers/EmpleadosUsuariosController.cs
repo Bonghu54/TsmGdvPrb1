46608faae5af7b0529425021d678ab62e6007a65
Bonghu54 <elwea1969ka@gmail.com>
2023-11-22 02:22:12 -0400

    prueba de insersion de datos de empleados y usuarios a la vez

---
+++ Controllers/EmpleadosUsuariosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TsmGdvPrb1.Data;
+using TsmGdvPrb1.Models;
+using TsmGdvPrb1.ViewModels;
+
+namespace TsmGdvPrb1.Controllers
+{
+    public class EmpleadosUsuariosController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EmpleadosUsuariosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var empleados = _context.Empleados.Include(e => e.usuario).ToList();
+            return View(empleados);
+        }
+
+
+        // POST: Registro
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Empleado empleado, Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.fechahora_registro = DateTime.Now;
+
+                empleado.usuario = usuario;
+
+                _context.Empleados.Add(empleado);
+                _context.Usuarios.Add(usuario);
+
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ViewModels/RegistroEmpleadosUsuariosViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TsmGdvPrb1.ViewModels
+{
+    public class RegistroEmpleadosUsuariosViewModel
+    {
+        // Propiedades para el empleado
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string CI { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaContratacion { get; set; }
+
+        // Propiedades para el usuario
+        [Required]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; }
+
+        [Required]
+        [Compare("Contrasena", ErrorMessage = "Las contraseñas no coinciden.")]
+        [DataType(DataType.Password)]
+        public string ConfirmarContrasena { get; set; }
+    }
+}
