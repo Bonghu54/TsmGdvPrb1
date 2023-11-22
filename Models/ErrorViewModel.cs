3e93b3cc94e8d5a9aac8dfca672d5fa3805780c0
Bonghu54 <elwea1969ka@gmail.com>
2023-11-21 22:37:53 -0400

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TsmGdvPrb1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
